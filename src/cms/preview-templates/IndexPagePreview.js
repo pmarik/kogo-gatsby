e2b1ec9dd7ad0d8c407fdc8bbceba5a5fbcfdffc
pmarik <pmarik@uwalumni.com>
2020-04-05 19:07:51 -0500

    updated data for indexPagePreview cms

--- src/cms/preview-templates/IndexPagePreview.js
+++ src/cms/preview-templates/IndexPagePreview.js
@@ -14,11 +14,11 @@ const IndexPagePreview = ({ entry, getAsset }) => {
         subheading={data.subheading}
         // mainpitch={data.mainpitch || {}}
         mainpitch={{
-          title: data.getIn(['data', 'mainpitch', 'title']),
-          description: data.getIn(['data', 'mainpitch', 'description']),
-          title_2: data.getIn(['data', 'mainpitch', 'title_2']),
-          description_2: data.getIn(['data', 'mainpitch', 'description_2']),
-          button_text: data.getIn(['data', 'mainpitch', 'button_text']),
+          title: data.title,
+          description: data.description,
+          title_2: data.title_2,
+          description_2: data.description_2,
+          button_text: data.button_text,
           image1: {
             image: getAsset(entry.getIn(['data', 'mainpitch', 'image1', 'image'])),
             alt: entry.getIn(['data', 'mainpitch', 'image1', 'alt']),
