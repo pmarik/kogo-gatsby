be60905afeddf0c5aa482a24046d9d2ce2e28f66
pmarik <pmarik@uwalumni.com>
2020-04-09 15:30:22 -0500

    updated title and content fonts montserrat and roboto

--- src/components/BlogRoll/BlogRoll.js
+++ src/components/BlogRoll/BlogRoll.js
@@ -37,8 +37,7 @@ class BlogRoll extends React.Component {
                     >
                       {post.frontmatter.title}
                     </Link>
-                    <span> &bull; </span>
-                    <span className="subtitle is-size-5 is-block">
+                    <span className="subtitle is-size-6 is-block">
                       {post.frontmatter.date}
                     </span>
                   </p>
