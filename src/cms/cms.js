a1c7563c781792d804f400d51b83e8f0154a9c8c
pmarik <pmarik@uwalumni.com>
2020-04-23 15:04:09 -0500

    test 2 uuid for product post variant

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,18 +1,18 @@
 import CMS from 'netlify-cms-app'
 import uploadcare from 'netlify-cms-media-library-uploadcare'
 import cloudinary from 'netlify-cms-media-library-cloudinary'
-//import idWidget from 'netlify-cms-widget-simple-uuid';
+import idWidget from 'netlify-cms-widget-simple-uuid';
 
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
-//import ProductPagePreview from './preview-templates/ProductPagePreview'
+import ProductPagePreview from './preview-templates/ProductPagePreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
 import ProductPostPreview from './preview-templates/ProductPostPreview';
 
 CMS.registerMediaLibrary(uploadcare)
 CMS.registerMediaLibrary(cloudinary)
 
-//CMS.registerWidget('id', idWidget.Control, idWidget.Preview);
+CMS.registerWidget('id', idWidget.Control, idWidget.Preview);
 
 CMS.registerPreviewTemplate('index', IndexPagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
