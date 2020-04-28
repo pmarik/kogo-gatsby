d613fa01a628b0237fc665b3a40b7180d5b6594c
pmarik <pmarik@uwalumni.com>
2020-04-28 12:45:14 -0500

    updated state field error on build for footer form

--- src/components/Footer/FooterForm.jsx
+++ src/components/Footer/FooterForm.jsx
@@ -51,7 +51,7 @@ class FooterForm extends React.Component{
                     <input ref="form-name" type="hidden" name="form-name" value="Newsletter Form" />
                         {/* <!-- ... --> */}
 
-                    <input ref="email" type="email" name="email" className={`${isDisabled}`} placeholder="Your Email Here" required className="email-input"/>
+                    <input ref="email" type="email" name="email" className={`${this.state.isDisabled}`} placeholder="Your Email Here" required className="email-input"/>
                         {/* <!-- ... --> */}
                     {/* <textarea ref="message" name="message" /> */}
                         {/* <!-- ... --> */}
